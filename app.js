ae8b8c8c3100ebb1cd24acd689f396be8b028703
宋轶 <songy@ciicsh.com>
2016-12-23 18:51:13 +0800

    add destroy and create function

--- app.js
+++ app.js
@@ -23,35 +23,35 @@ var Emp = ds.createModel('crm.contact',{
 });
 
 
-Emp.find(
-    {
-        where:{
-            "and":[
-                {"id":"1"},
-                {"last_name":{"neq" :"test2"}},
-                {"or" :[{"first_name":"last"},{"title":"IT1"}]}
-            ]
-        },
-        // where: {"id":"1"},
-        // where: {id:{
-        //     nin:[1,2]
-        // }},
-        // where:{
-        //     "or":[
-        //         {"id":"1"},
-        //         {"last_name":"test1"}
-        //     ]
-        // },
-        fields: {first_name:true,last_name:true,title:true},
-        limit:  1,
-        order:  'last_name DESC',//['last_name DESC','first_name ASC']
-        skip:   0}
-        ,function (err, emp) {
-    if(err) throw err;
-    // console.log(ds.connector);
-     console.dir(emp);
-});
-    //
+// Emp.find(
+//     {
+//         where:{
+//             "and":[
+//                 {"id":"1"},
+//                 {"last_name":{"neq" :"test2"}},
+//                 {"or" :[{"first_name":"last"},{"title":"IT1"}]}
+//             ]
+//         },
+//         // where: {"id":"1"},
+//         // where: {id:{
+//         //     nin:[1,2]
+//         // }},
+//         // where:{
+//         //     "or":[
+//         //         {"id":"1"},
+//         //         {"last_name":"test1"}
+//         //     ]
+//         // },
+//         fields: {first_name:true,last_name:true,title:true},
+//         limit:  1,
+//         order:  'last_name DESC',//['last_name DESC','first_name ASC']
+//         skip:   0}
+//         ,function (err, emp) {
+//     if(err) throw err;
+//     // console.log(ds.connector);
+//      console.dir(emp);
+// });
+
     // Emp.create({
     //     first_name:'test2',
     //     last_name:'last',
@@ -61,12 +61,12 @@ Emp.find(
     //     console.dir(response);
     // });
     //
-    // Emp.destroyById(1
-    // ,function (err,response) {
-    //     if(err) throw err;
-    //     console.dir(response);
-    // });
-    //
+    Emp.destroyById(2
+    ,function (err,response) {
+        if(err) throw err;
+        console.dir(response);
+    });
+
     // Emp.destroyAll({where:{id:1}}
     //     ,function (err,response) {
     //         if(err) throw err;
