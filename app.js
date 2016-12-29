08b7d477668481e4e4cff4793d1f917c8261b319
宋轶 <songy@ciicsh.com>
2016-12-29 16:12:42 +0800

    modify all function field  and order

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var ds = loopback.createDataSource({
     password : 'admin',
     db : 'song',
     host : 'localhost',
-    port: '8069'
+    port: '8069',
+    debug: true
 });
 
 
@@ -23,48 +24,50 @@ var Emp = ds.createModel('crm.contact',{
 });
 
 
-// Emp.find(
-//     {
-//         where:{
-//             "and":[
-//                 {"id":"1"},
-//                 {"last_name":{"neq" :"test2"}},
-//                 {"or" :[{"first_name":"last"},{"title":"IT1"}]}
-//             ]
-//         },
-//         // where: {"id":"1"},
-//         // where: {id:{
-//         //     nin:[1,2]
-//         // }},
-//         // where:{
-//         //     "or":[
-//         //         {"id":"1"},
-//         //         {"last_name":"test1"}
-//         //     ]
-//         // },
-//         fields: {first_name:true,last_name:true,title:true},
-//         limit:  1,
-//         order:  'last_name DESC',//['last_name DESC','first_name ASC']
-//         skip:   0}
-//         ,function (err, emp) {
-//     if(err) throw err;
-//     // console.log(ds.connector);
-//      console.dir(emp);
-// });
+Emp.find(
+    {
+        // where:{
+        //     "and":[
+        //         {"id":"1"},
+        //         {"last_name":{"neq" :"test2"}},
+        //         {"or" :[{"first_name":"last"},{"title":"IT1"}]}
+        //     ]
+        // },
+        // where: {"id":"1"},
+        where: {id:{
+            nin:[1,2]
+        }},
+        // where:{
+        //     "or":[
+        //         {"id":"1"},
+        //         {"last_name":"test1"}
+        //     ]
+        // },
+        fields: {first_name:true,last_name:true,title:true},
+        // fields: {title:false},
+        limit:  1,
+        order:  'last_name DESC',//['last_name DESC','first_name ASC']
+        // order:  ['last_name DESC','first_name ASC'],
+        skip:   0}
+        ,function (err, emp) {
+    if(err) throw err;
+    // console.log(ds.connector);
+     console.dir(emp);
+});
 
-        Emp.create({
-            first_name:'test2',
-            last_name:'last',
-            title:'sales'
-            },function (err,response) {
-            if(err) throw err;
-            console.dir(response);
-            response.updateAttribute('first_name','updated2',function (err, res) {
-                if(err) throw err;
-                console.dir(res);
-            })
-        });
-    //
+        // Emp.create({
+        //     first_name:'test2',
+        //     last_name:'last',
+        //     title:'sales'
+        //     },function (err,response) {
+        //     if(err) throw err;
+        //     console.dir(response);
+        //     response.updateAttribute('first_name','updated2',function (err, res) {
+        //         if(err) throw err;
+        //         console.dir(res);
+        //     })
+        // });
+    // //
     // Emp.destroyById(2
     // ,function (err,response) {
     //     if(err) throw err;
