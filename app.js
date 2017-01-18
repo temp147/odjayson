10f6e5fda9a53776427ba956a841d82948ca36da
宋轶 <songy@ciicsh.com>
2017-01-18 18:50:41 +0800

    implement the update function

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var loopback = require('loopback');
 var ds = loopback.createDataSource({
     connector: require("./index"),
     login : 'admin',
-    password : 'admin1',
+    password : 'admin',
     db : 'song',
     host : 'localhost',
     port: '8069',
@@ -24,70 +24,70 @@ var Emp = ds.createModel('crm.contact',{
 });
 
 
-Emp.find(
-    {
-                // where:{
-                //     "and":[
-                //         {"id":"1"},
-                //         {"last_name":{"neq" :"test2"}},
-                //         {"last_name": "test3"},
-                //         {"or" :[
-                //             {"first_name":"last"},
-                //             {"title":"IT1"},
-                //             {"and":[
-                //                 {"id":"3"},
-                //                 {"last_name":{"neq" :"test3"}},
-                //                 {"last_name": "test1"}
-                //             ]}
-                //             ]}
-                //     ]
-                // },
-        // where: {"id":"1"},
-        // where: {id:{
-        //     nin:[1,2]
-        // }},
-        // where:{
-        //     "or":[
-        //         {"id":"1"}
-        //     ]
-        // },
-        where:{
-           "and":[
-                    {"id":"6"},
-                    {"last_name":{"neq" :"test3"}},
-                    {"or":[{"first_name":"last"},{"title":"IT1"}]}
-                ]
-        },
-        // where:{
-        //   "or":[
-        //       {"and":[
-        //           {"or":[
-        //               {a:5},
-        //               {b:6}
-        //           ]},
-        //           {c:7}
-        //       ]},
-        //       {"and":[
-        //           {d:1},
-        //           {e:2}
-        //       ]},
-        //       {"and":[
-        //           {f:3},
-        //           {g:4}
-        //       ]}
-        //   ]
-        // },
-        fields: {first_name:true,last_name:true,title:false},
-        // fields: {title:false},
-        limit:  1,
-        order:  'last_name DESC',//['last_name DESC','first_name ASC']
-        // order:  ['last_name DESC','first_name ASC'],
-        skip:   0}
-        ,function (err, emp) {
-    if(err) throw err;
-    // console.log(ds.connector);
-     console.dir(emp);
-});
+// Emp.find(
+//     {
+//                 // where:{
+//                 //     "and":[
+//                 //         {"id":"1"},
+//                 //         {"last_name":{"neq" :"test2"}},
+//                 //         {"last_name": "test3"},
+//                 //         {"or" :[
+//                 //             {"first_name":"last"},
+//                 //             {"title":"IT1"},
+//                 //             {"and":[
+//                 //                 {"id":"3"},
+//                 //                 {"last_name":{"neq" :"test3"}},
+//                 //                 {"last_name": "test1"}
+//                 //             ]}
+//                 //             ]}
+//                 //     ]
+//                 // },
+//         // where: {"id":"1"},
+//         // where: {id:{
+//         //     nin:[1,2]
+//         // }},
+//         // where:{
+//         //     "or":[
+//         //         {"id":"1"}
+//         //     ]
+//         // },
+//         where:{
+//            "and":[
+//                     {"id":"6"},
+//                     {"last_name":{"neq" :"test3"}},
+//                     {"or":[{"first_name":"last"},{"title":"IT1"}]}
+//                 ]
+//         },
+//         // where:{
+//         //   "or":[
+//         //       {"and":[
+//         //           {"or":[
+//         //               {a:5},
+//         //               {b:6}
+//         //           ]},
+//         //           {c:7}
+//         //       ]},
+//         //       {"and":[
+//         //           {d:1},
+//         //           {e:2}
+//         //       ]},
+//         //       {"and":[
+//         //           {f:3},
+//         //           {g:4}
+//         //       ]}
+//         //   ]
+//         // },
+//         fields: {first_name:true,last_name:true,title:false},
+//         // fields: {title:false},
+//         limit:  1,
+//         order:  'last_name DESC',//['last_name DESC','first_name ASC']
+//         // order:  ['last_name DESC','first_name ASC'],
+//         skip:   0}
+//         ,function (err, emp) {
+//     if(err) throw err;
+//     // console.log(ds.connector);
+//      console.dir(emp);
+// });
 
         // Emp.create({
         //     first_name:'test2',
@@ -120,3 +120,16 @@ Emp.find(
     //         console.dir(response);
     //     }
     // )
+Emp.create({
+    first_name:'test2',
+    last_name:'last',
+    title:'sales'
+    },function (err,response) {
+    if(err) throw err;
+    console.dir(response);
+    console.dir(response.first_name);
+    response.save(function (err, res) {
+        if(err) throw err;
+        console.dir('save done')
+    })
+});
